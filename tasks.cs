306ae36d415e0e89edd396c597eeaf6311d9234f
isjcode <98400721+isjcode@users.noreply.github.com>
2022-02-09 18:16:10 -0800

    Add files via upload

---
+++ tasks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+    
+
+        }
+
+        #region task2
+        static void task2(string[] s, char c)
+        {
+            foreach(string str in s)
+            {
+                foreach(char c2 in str)
+                {
+                    if (c == c2)
+                    {
+                        Console.WriteLine(str);
+                        break;
+                    }
+                }
+            }
+        }
+        #endregion
+        #region task1
+        static void CustomSort(int[] arr)
+        {
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+        #endregion
+        
+    }
+}
